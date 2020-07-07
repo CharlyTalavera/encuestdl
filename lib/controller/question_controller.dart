0a84c7719283710a58000830c14201dc46f6747f
Charly Talavera <talaveracarlosd@gmail.com>
2020-07-06 21:49:47 -0300

    checks added before create a question

--- lib/controller/question_controller.dart
+++ lib/controller/question_controller.dart
@@ -1,5 +1,6 @@
 import 'package:encuestdl/encuestdl.dart';
 import 'package:encuestdl/model/question.dart';
+import 'package:encuestdl/model/poll.dart';
 
 class QuestionController extends ResourceController {
   final ManagedContext context;
@@ -30,22 +31,37 @@ class QuestionController extends ResourceController {
 
   @Operation.post()
   Future<Response> createQuestion() async {
-      final Map<String, dynamic> body = await request.body.decode();
-      if(body['value']?.isEmpty?? true)
-        Response.badRequest(body: {"error": "Missing required param 'value'"});
-      if(body['correct']?.isEmpty?? true)
-        Response.badRequest(body: {"error": "Missing required param 'correct'"});
-      if(body['options']?.isEmpty?? true)
-        Response.badRequest(body: {"error": "Missing required param 'options'"});
+    final Map<String, dynamic> body = await request.body.decode();
+    if(body == null)
+      return Response.badRequest(body: {"error": "Missing body"});
+    if(body['value']?.isEmpty?? true)
+      return Response.badRequest(body: {"error": "Missing required param 'value'"});
+    if(body['correct']?.isEmpty?? true)
+      return Response.badRequest(body: {"error": "Missing required param 'correct'"});
+    if(body['options']?.isEmpty?? true)
+      return Response.badRequest(body: {"error": "Missing required param 'options'"});
+    if(body['poll']?.isEmpty?? true)
+      return Response.badRequest(body: {"error": "Missing required param 'poll' id"});
+    
+    final pollId = int.parse(body['poll'][0]);
+    final pollQuery = Query<Poll>(context)
+      ..where((p) => p.id).equalTo(pollId);
 
-      final query = Query<Question>(context)
-        ..values.value = body['value'][0]
-        ..values.poll.id = int.parse(body['poll'][0])
-        ..values.correct = int.parse(body['correct'][0])
-        ..values.options = Document(body['options']);
+    final poll = await pollQuery.fetchOne();
 
-      final insertedQuestion = await query.insert();
-
-      return Response.ok(insertedQuestion);
+    if(poll == null)
+      return Response.badRequest(body: {"error": "The poll id ${pollId} is invalid"});
+    final correct = int.parse(body['correct'][0]);
+    if(correct < 1 && correct > body['options'].length)
+      return Response.forbidden(body: {"error": "Correct option must be between 1 and ${body['options'].length}"});
+    
+    final query = Query<Question>(context)
+      ..values.value = body['value'][0]
+      ..values.poll.id = int.parse(body['poll'][0])
+      ..values.correct = int.parse(body['correct'][0])
+      ..values.options = Document(body['options']);
+    
+    final insertedQuestion = await query.insert();
+    return Response.ok(insertedQuestion);
   }
 }
